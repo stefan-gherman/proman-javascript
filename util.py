66e2176b1fcf2d6979ab3bdd152ddeda3148f92c
Claudiu <cduneata@gmail.com>
2020-02-19 09:25:50 +0200

    Add hash password

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask import jsonify
+import bcrypt
 
 
 def json_response(func):
@@ -8,8 +9,21 @@ def json_response(func):
     :param func:
     :return:
     """
+
     @wraps(func)
     def decorated_function(*args, **kwargs):
         return jsonify(func(*args, **kwargs))
 
     return decorated_function
+
+
+def hash_password(password_text):
+    hashed_bytes = bcrypt.hashpw(password_text.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(password_text, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password_text.encode('utf-8'), hashed_bytes_password)
+
+
