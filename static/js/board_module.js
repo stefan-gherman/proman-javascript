f128544f333257f037415ebeb65a417432680ac4
Claudiu <cduneata@gmail.com>
2020-03-04 14:51:47 +0200

    Fix board title rename bug

--- static/js/board_module.js
+++ static/js/board_module.js
@@ -1,6 +1,7 @@
 export function addEventClickBoardTitle() {
     let allBoardsTitle = document.querySelectorAll('#board-title');
     for (let boardTitle of allBoardsTitle) {
+
         if (boardTitle.id.includes('board-title')) {
             boardTitle.addEventListener('click', handleBoardTitle);
             boardTitle.addEventListener('keydown', handleBoardTitleOnKeyPress);
@@ -10,10 +11,15 @@ export function addEventClickBoardTitle() {
 
 
 function handleBoardTitleOnKeyPress(event) {
+    console.log(event.target)
     if (event.which == 13 || event.keyCode == 13) {
         // event.target.defaultValue = event.target.value;
-        console.log(event.target.value);
         let tempValue = event.target.value;
+        console.log(event.target.textContent.trim())
+        if (tempValue.trim() === '') {
+            console.log("tempval" + tempValue)
+            tempValue = event.target.defaultValue;
+        }
         event.currentTarget.innerHTML = tempValue;
         let boardId = event.currentTarget.dataset.boardId;
         let data = {boardId: boardId, tempValue: tempValue};
@@ -24,7 +30,7 @@ function handleBoardTitleOnKeyPress(event) {
         fetch(`http://127.0.0.1:5000/api/rename-board-title/${boardId}`, options);
 
     } else if (event.which == 27 || event.keyCode == 27) {
-        event.currentTarget.innerHTML = event.currentTarget.dataset.boardTitle;
+        event.currentTarget.innerHTML = event.target.defaultValue;;
     }
 }
 
@@ -32,15 +38,14 @@ function handleBoardTitleOnKeyPress(event) {
 function handleBoardTitle(event) {
     event.target.innerHTML = `
     <input type="text" class="form-control" id="board-rename-input" autofocus 
-    value="${event.currentTarget.dataset.boardTitle}">
-    `
+    value="${event.target.textContent.trim()}">`
+
 }
 
 
 export function handleDeleteClick(event) {
     let board_id = event.target.id.slice(24);
     let data = {board_id};
-    console.log(data);
     const option = {
         method: 'POST',
         headers: {
